7c37131e5fc9ccd23dd99119de23e3ea052c79a3
Robert Duddy <rduddy13@gmail.com>
2022-11-12 08:49:38 -0500

    replaced callback hell with promises

--- operations.js
+++ operations.js
@@ -1,34 +1,20 @@
-const assert = require("assert").strict;
-
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection);
-  coll.find().toArray((err, docs) => {
-    assert.strictEqual(err, null);
-    callback(docs);
-  });
+  return coll.find().toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.deleteOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.deleteOne(document);
 };
 
 // third parameter on updateOne is to pass in optional configurations, use null if you dont need them
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
   const coll = db.collection(collection);
-  coll.updateOne(document, { $set: update }, null, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.updateOne(document, { $set: update }, null);
 };
